db2d807124b1af748c77b253e6d07551c6f72fe0
AkwasiAppiah <akwasi.atta.appiah@gmail.co.uk>
2021-12-29 18:25:58 +0000

    Created password input box and ability to lock and unlock

--- src/components/PasswordComparer/PasswordSetter.tsx
+++ src/components/PasswordComparer/PasswordSetter.tsx
@@ -1,23 +1,34 @@
-import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { Button, HStack, Input, InputGroup, InputRightElement, } from '@chakra-ui/react';
 import React from 'react';
 
 const PasswordSetter = (): JSX.Element => {
     const [show, setShow] = React.useState(false)
+    const [disabled, setDisabled] = React.useState(false)
+
     const handleShowClick = () => setShow(!show)
-  
+    const handleDisabledClick = () => setDisabled(!disabled)
+
     return (
-      <InputGroup size='md'>
-        <Input
-          pr='4.5rem'
-          type={show ? 'text' : 'password'}
-          placeholder='Enter password'
-        />
-        <InputRightElement width='4.5rem'>
-          <Button h='1.75rem' size='sm' onClick={handleShowClick}>
-            {show ? 'Hide' : 'Show'}
-          </Button>
-        </InputRightElement>
-      </InputGroup>
+
+      <HStack>
+        <Button h='1.75rem' size='sm' onClick={handleDisabledClick}>
+
+          {disabled ? <i className="fas fa-lock-open"></i> : <i className="fas fa-lock"></i>}
+        </Button>
+        <InputGroup size='md'>
+          <Input
+            pr='4.5rem'
+            type={show ? 'text' : 'password'}
+            placeholder='Enter password'
+            disabled = {disabled}
+          />
+          <InputRightElement width='4.5rem'>
+            <Button h='1.75rem' size='sm' onClick={handleShowClick}>
+              {show ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </HStack>
     )
     }
 export default PasswordSetter
